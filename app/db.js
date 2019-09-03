78213526de393631ae5c9e2ffb47bb1cfd84833c
melaniepeu <49374642+melaniepeu@users.noreply.github.com>
2019-09-03 16:06:16 +0200

    clevercloud

--- app/db.js
+++ app/db.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
+    host     : 'be9uokie3lhefdpnj3ss-mysql.services.clever-cloud.com',
+    user     : 'urgbcj8tzeyizrf6',
+    password : '7PTzCmXh2uoN4jVOkdKe',
     port     : '3306',
-    database : 'facturationdb'
+    database : 'be9uokie3lhefdpnj3ss'
 });
 
 module.exports = connection;
